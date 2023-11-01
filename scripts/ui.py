865c57ccef299a91aa377738bd636bacbc56a945
junha-lee <junha4304@gmail.com>
2023-11-01 16:09:46 +0900

    [feat] small_points_remover

--- scripts/ui.py
+++ scripts/ui.py
@@ -57,7 +57,29 @@ def gaussian_and_re_threshold(image:Image.Image, threshold:int)->Image.Image:
     new_image = Image.fromarray(new_image)
     return new_image
 
+def small_points_remover(image:Image.Image, threshold:int) -> Image.Image:
+    # convert RGBA to cv2.COLOR_BGR2GRAY
+    white_image = Image.new("RGB", image.size, (255, 255, 255))
+    white_image.paste(image, mask=image.split()[3])
+    grayscale_image = cv2.cvtColor(np.array(white_image), cv2.COLOR_BGR2GRAY)
     
+    ret, thresh = cv2.threshold(grayscale_image, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # contour의 가로외 세로중에 큰 값이 100이상인 것만 추출
+    contours = [i for i in contours if cv2.boundingRect(i)[2] > threshold or cv2.boundingRect(i)[3] > threshold]
+    
+    mask = np.zeros_like(white_image)
+    cv2.drawContours(mask, contours, -1, (255,255,255), -1)
+    
+    # convert mask to RGBA
+    apng_shape = (image.height, image.width, 4)
+    new_image = np.zeros(apng_shape, dtype=np.uint8)
+    # put the mask's black pixels to new image
+    new_image[mask[:,:,0] == 0] = [0,0,0,255]
+    new_image = Image.fromarray(new_image)
+    return new_image
+
 
 def on_ui_tab_called():
     with gr.Blocks() as transparent_interface:
@@ -66,18 +88,20 @@ def on_ui_tab_called():
                 with gr.TabItem("PNG2APNG"):
                     with gr.Row():
                         image_upload_input = gr.Image(label="Upload Image", source= "upload",type="pil")
-                        threshold_input = gr.Slider(minimum=0, maximum=255, value=100, label="Threshold")
+                        threshold_input = gr.Slider(minimum=0, maximum=255, value=100, label="Threshold_black")
+                        threshold_remove = gr.Slider(minimum=0, maximum=255, value=50, label="Threshold_remove")
                         upscale_input = gr.Slider(minimum=1, maximum=8, value=1, label="Upscale, 1 to disable")
                         adaptive_checkbox = gr.Checkbox(label="Adaptive Threshold", value=False)
                         button = gr.Button(label="Convert")
                     with gr.Row():
                         image_upload_output = gr.Image(label="Output Image",type="numpy")
-                    def convert_image(image:Image.Image, threshold:float, upscale_scale:float, adaptive:bool)->np.ndarray:
+                    def convert_image(image:Image.Image, threshold:float, remove:float, upscale_scale:float, adaptive:bool)->np.ndarray:
                         """
                         Converts the image to apng
                         The black color (with some threshold) will remain, others will be transparent
                         """
                         color_threshold = threshold
+                        remove_threshold = remove
                         # upscale the image
                         # convert to RGB
                         image = upscale_image(image, upscale_scale)
@@ -121,8 +145,10 @@ def on_ui_tab_called():
                             new_image[threshol_result == 0] = [0,0,0,255]
                             new_image = Image.fromarray(new_image)
                             new_image = gaussian_and_re_threshold(new_image, color_threshold)
+                            new_image = small_points_remover(new_image, remove_threshold)
                         return new_image # return the new image
-                    button.click(convert_image, inputs=[image_upload_input, threshold_input, upscale_input, adaptive_checkbox], outputs=[image_upload_output])
+                    
+                    button.click(convert_image, inputs=[image_upload_input, threshold_input, threshold_remove, upscale_input, adaptive_checkbox], outputs=[image_upload_output])
     return (transparent_interface, "PNG2APNG", "script_png2apng_interface"),
 
 try:
